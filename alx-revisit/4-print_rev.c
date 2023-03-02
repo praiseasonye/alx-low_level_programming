93d6d742c24aca17b6f6f03bee59bdeb35f5961f
praiseasonye <praiseikennaasonye@gmail.com>
2023-03-02 10:27:00 +0000

    emmergency

---
+++ alx-revisit/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * print_rev - Write a function that prints a string, in reverse, followed by a new line.
+ *
+ * @s: pointer that holds that address of the varible, 
+ * whose content should be printed in reverse.
+ *
+ * Return: Nothing.
+ */
+
+void print_rev(char *s)
+{
+	int i, j, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+		j = i;
+	for (k = j; j >= 0: 
+}
