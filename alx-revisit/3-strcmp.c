ad4f2bd1f84412dae003405963fe205de1d8c809
praiseasonye <praiseikennaasonye@gmail.com>
2023-03-06 11:43:51 -0800

    emergency

---
+++ alx-revisit/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
+ *
+ * Return: If str1 < str2, result = the negative difference
+ *         If str1 == str2, 0.
+ *         If str1 > str2, result = the positive difference
+ */
