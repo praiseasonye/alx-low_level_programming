e250cc7b070eefed9358d342831822359b781aea
praiseasonye <praiseikennaasonye@gmail.com>
2023-03-06 21:54:15 +0000

    memset

---
+++ alx-revisit/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * A function that copies the string pointed to by src, including
+ * the terminating null byte (\0), to the buffer pointed to by dest.
+ *
+ * @src: pointer to the variable that holds the string to be copied
+ * @dest: the destination variable were the string is to be copied to.
+ *
+ * Return: the dest variable.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while(src[i] != '\0')
+	{
+		dest[i] = src[i];
+	}
+	src[i] = '\0';
+	return (dest);
+}
