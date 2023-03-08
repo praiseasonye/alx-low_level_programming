af898f0727d6cf1eaab68f78aaddacb771a02f83
praiseasonye <praiseikennaasonye@gmail.com>
2023-03-07 16:26:04 -0800

    emergency

---
+++ alx-revisit/7-leet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * leet - A function that encodes a string into 1337.
+ *
+ * @str: a pointer that hold the address of the variable
+ * containing the string to be coded.
+ *
+ * Return: A pointer that holds the address of of the 
+ * codded string.
+ */
+
+char *leet(char *)
+{
+
+}
