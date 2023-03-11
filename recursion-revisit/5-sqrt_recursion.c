31a36197a4d6b06c216338c7451db6027443042a
praiseasonye <praiseikennaasonye@gmail.com>
2023-03-11 11:02:27 +0000

    m

---
+++ recursion-revisit/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns the value of u raised to the power of f
+ * @u: the value to raise
+ * @f: raised to the power
+ *
+ * Return: the result of the power
+ */
+
+int _pow_recursion(int u, int f)
+{
+	if (f < 0)
+		return (-1);
+	if (f == 0)
+		return (1);
+	return (u * _pow_recursion(u, f - 1));
+}
+
+int iterate(int *n)
+	if (n == 0)
+		return (n);
+	return iterate(n - 1);
