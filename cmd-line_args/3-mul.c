189193ae9c5a3beddb94d35351ab7ac91193fd54
praiseasonye <praiseikennaasonye@gmail.com>
2023-03-16 06:31:57 -0700

    Neither irony nor sarcasm is argument

---
+++ cmd-line_args/3-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that multiplies two numbers.
+ * the program prints the result of the multiplication,
+ * followed by a new line.
+ * the two numbers and result of the multiplication
+ * can be stored in an integer
+ * If the program does not receive two arguments,
+ * your program should print Error,
+ * followed by a new line, and return 1
+ *
