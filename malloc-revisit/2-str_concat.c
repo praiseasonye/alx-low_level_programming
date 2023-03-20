167866ca0993a161362af2d7786ab53de7338fcb
praiseasonye <praiseikennaasonye@gmail.com>
2023-03-20 04:05:45 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- malloc-revisit/2-str_concat.c
+++ malloc-revisit/2-str_concat.c
@@ -18,6 +18,10 @@ char *str_concat(char *s1, char *s2)
 	int str1_len = 0;
 	int str2_len = 1;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		str1_len += 1;
 
