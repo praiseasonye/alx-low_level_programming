547477d4adfbb1aa8371334324d97cdc4c981437
praiseasonye <praiseikennaasonye@gmail.com>
2023-04-08 04:06:55 -0700

    strpbrk

--- more_pointers/4-strpbrk.c
+++ more_pointers/4-strpbrk.c
@@ -20,10 +20,8 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
-				break;
+				return (s + i);
 		}
-		if (s[i] == '\0')
-			return (NULL);
 	}
-	return (s + i);
+	return (NULL);
 }
