6404d65fe3a71f33abf3a6c58672eaa0434062ea
praiseasonye <praiseikennaasonye@gmail.com>
2023-04-08 03:00:12 -0700

    strpbrk

---
+++ more_pointers/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - s function that returns a pointer to the first
+ * occurence in the string s of any bytes in the string accept
+ * returns null if no match is found.
+ *
+ * @s: the string to be scanned for a match
+ * @accept: the characters the should match the strings in s
+ *
+ * Return: a char pointer to the first occurence in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (i = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (s[i] == '\0')
+			return (NULL);
+	}
+	return (s);
+}
