5dc478230422f0962a2b4ac36e9e7abc461dffe9
praiseasonye <praiseikennaasonye@gmail.com>
2023-04-30 20:56:26 +0000

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,8 +11,6 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
 	if (*s == '\0')
 	{
 		_putchar('\n');
@@ -20,6 +18,5 @@ void _puts_recursion(char *s)
 	}
 
 	_putchar(*s);
-	i++;
 	_puts_recursion(s + i);
 }
