5c3f3f1e92634b189b2c558ecd3e70beb4e20681
praiseasonye <praiseikennaasonye@gmail.com>
2023-05-14 19:12:26 +0000

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a function that does simple arithmetic on two integars
+ *        from the command line
+ * @argc: the number of arguments passe go the main function
+ * @argv: an array containing the inputs entered by the user
+ *
+ * Return: the result of the operation performed by the function or
+ *         Error if the arguments are invlid or a wrng operator is
+ *         to the command line.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((**(argv + 2) == '%' || *argv[2] == '/') && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+
+/**
+ * op_add - a function that adds two integars
+ *
+ * @a: the first integar
+ * @b: the second integar
+ *
+ * Return: the sum of the two integars
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - a function that subtracts an integar
+ *          from another
+ *
+ * @a: the first integar
+ * @b: the second integar
+ *
+ * Return: deduct b from a.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function that multiplies two integars
+ *
+ * @a: the first integar
+ * @b: the second integar
+ *
+ * Return: multiply a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function that divides a with b
+ *
+ * @a: the first integar
+ * @b: the second integar
+ *
+ * Return: a divided by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a function that returns the remaider
+ *          after divideing a with b
+ *
+ * @a: the first integar
+ * @b: the second integar
+ *
+ * Return: the remainder aftter a is divided by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
