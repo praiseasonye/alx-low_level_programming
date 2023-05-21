2a2433603a8b46fea4507385f60104dee1d3c00e
praiseasonye <praiseikennaasonye@gmail.com>
2023-05-21 14:20:58 +0000

    updated herader file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * struct args - a structure that holds the differnt data types
+ *
+ * @c: char
+ * @i: int
+ * @f: float
+ * @s: string
+ *
+ */
+
+struct args
+{
+	char c;
+	int i;
+	float f;
+	char *s;
+}
+
+/**
+ * struct format_data - a structure that will be used to
+ *                      iterate over the specifiers and thier data
+ *                      types.
+ * @specifier: the letter indicating a datatype
+ * @data_args: these are the diferent datatypes. the diferent args
+ *             would be stored here.
+ */
+
+typedef struct format_data
+{
+	char specifier;
+	struct args data_args;
+} fd_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
