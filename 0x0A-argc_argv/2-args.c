9f77727eaa4717dd493889934ebab3335dcfb7af
praiseasonye <praiseikennaasonye@gmail.com>
2023-06-12 08:28:40 -0700

    The best argument against democracy is a five-minute conversation with the average voter

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,7 +5,7 @@
  * main - a function that prints att the argument it receives
  *
  * @argc: the total number argument passed to the function
- * @argv: an array of pointers to the strings passed to the 
+ * @argv: an array of pointers to the strings passed to the
  *        command line as argument.
  *
  * Return: always 0 if function runs successfully.
