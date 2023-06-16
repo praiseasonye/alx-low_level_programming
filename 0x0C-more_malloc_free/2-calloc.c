a58aadb26c844ec44ff27f2cc89f9908be0c7119
praiseasonye <praiseikennaasonye@gmail.com>
2023-06-16 22:22:36 +0000

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (size * nmemb); i++)
 		((char *)ptr)[i] = 0;
-	return ((void *)ptr);
+	return (ptr);
 }
