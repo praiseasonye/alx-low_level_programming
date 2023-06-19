8b7f5a7a3c33f6bb8ad06570ee57e34509cb6098
praiseasonye <praiseikennaasonye@gmail.com>
2023-06-19 19:59:39 +0000

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -48,47 +50,37 @@ void errors(void)
 	exit(98);
 }
 
-
 /**
- * main - A program that multiplies two positive numbers
- *        from the command line
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * @argc: the number of arguments passed to the command line
- * @argv: an array of pointers to the strings (argument) passed
- *        to the command line.
- *
- * Return: 0 if successful/
+ * Return: always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	char *arg1;
-	char *arg2;
-	int i, len1, len2, len, carry, digit1, digit2, *result, mul_checker = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	arg1 = argv[1];
-	arg2 = argv[3];
-	if (argc != 3 || arg1 == NULL || arg2 == NULL)
-	{
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	}
-
-	len1 = _strlen(arg1);
-	len2 = _strlen(arg2);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 > 0; len1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = arg1[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = len2 - 1 ; len >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = arg2[len2] - '0';
+			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
@@ -96,17 +88,16 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for(i = 0; i < len - 1; i++)
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
-			mul_checker = 1;
-		if (mul_checker)
+			a = 1;
+		if (a)
 			_putchar(result[i] + '0');
 	}
-	if (!mul_checker)
-		_putchar('\n');
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
 	free(result);
 	return (0);
-
-
 }
