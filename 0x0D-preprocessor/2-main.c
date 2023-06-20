dc2a8091658af1d54abc7bc39b53cbe9b93d380b
praiseasonye <praiseikennaasonye@gmail.com>
2023-06-20 21:16:21 +0000

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point. a program that prints the name
+ *        of the file it was compiled from, followed by
+ *        a new line.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
+
