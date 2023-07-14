752973a0040034e84ed2d8f4637dd6afee9a01c6
praiseasonye <praiseikennaasonye@gmail.com>
2023-07-14 15:42:28 +0000

    Under the snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /*main.h*/
