3efefb4834da5f4a5e730d5512ace6e647c0e32a
praiseasonye <praiseikennaasonye@gmail.com>
2023-07-14 13:09:00 +0000

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,11 +15,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t read_bytes, write_bytes;
 	char *buff;
-	int fd = open(filename, O_RDONLY);
+	int fd;
 
-	if (fd == -1)
-		return (0);
 	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 	buff = malloc(letters * sizeof(char));
 	if (buff == NULL)
@@ -34,11 +38,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	write_bytes = write(1, buff, read_bytes);
 	if (write_bytes == -1)
-	{
-		free(buff);
-		return (0);
-	}
-	if (write_bytes != read_bytes)
 	{
 		free(buff);
 		return (0);
