d6dfca21427e6c626f4c3831f543896bfdbc11bc
praiseasonye <praiseikennaasonye@gmail.com>
2023-08-05 06:30:07 +0000

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,10 +12,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	if ((sizeof(unsigned long int) * 8) <= index)
 		return (-1);
 
-	if ((n & (1 << index)) == 0)
+	if (n & (1 << index) == 0)
 		return (0);
 
 	return (1);
